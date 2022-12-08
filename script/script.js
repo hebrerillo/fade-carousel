c8b2057babf42112c896e107b919a17d1a398c4d
hebrerillo <hebrerillo@gmail.com>
2022-12-08 13:05:16 +0100

    Setting indexes for the carousel items and menu items

--- script/script.js
+++ script/script.js
@@ -2,12 +2,36 @@
 
 function SlideUnderline()
 {
+    this.carousel = document.querySelector('.ffcarousel');
     this.navigationMenu = document.querySelector('.nav');
     this.list = this.navigationMenu.querySelector('.nav__list');
     this.underline = this.navigationMenu.querySelector('.nav__underline');
     this.list.addEventListener('click', this.gotoMenuItemCB.bind(this));
     this.gotoMenuItem(this.list.querySelector('.nav__list__link'), false);
+    this.setIndexes();
 }
+
+/**
+ * Sets the necessary indexes in the menu items and in the target carousel childs
+ * 
+ */
+SlideUnderline.prototype.setIndexes = function()
+{
+    const carouselChildren = this.carousel.children;
+    const menuChildren = this.list.querySelectorAll('.nav__list__link');
+    
+    if (menuChildren.length !== carouselChildren.length)
+    {
+        console.error("SlideUnderline.prototype.setIndexes - The number of carousel items and the number of menu items are different");
+        return;
+    }
+    
+    for(let i = 0; i < menuChildren.length; ++i)
+    {
+        carouselChildren[i].dataset.carouselItem = i;
+        menuChildren[i].dataset.carouselTargetItem = i;
+    }
+};
 
 /**
  * The callback executed when a list item is clicked
