acca8a9835dc212f98b8d8ab61bfe2482b3fd606
hebrerillo <hebrerillo@gmail.com>
2022-12-29 11:33:31 +0100

    Refactoring the underline code

--- samples/script/script.js
+++ samples/script/script.js
@@ -4,7 +4,8 @@ function UnderlineSlide()
 {
     this.navigationMenu = document.querySelector('.nav');
     this.list = this.navigationMenu.querySelector('.nav__list');
-    this.underline = this.navigationMenu.querySelector('.nav__underline');
+    this.underline = this.navigationMenu.querySelector('#underline');
+    this.underline.addEventListener('transitionend', this.underlineTransitionEnd.bind(this));
     this.list.addEventListener('click', this.gotoMenuItemCB.bind(this));
     this.navigationMenu.querySelector('.left-button').addEventListener('click', this.gotoPreviousSlide.bind(this));
     this.navigationMenu.querySelector('.right-button').addEventListener('click', this.gotoNextSlide.bind(this));
@@ -18,6 +19,27 @@ function UnderlineSlide()
     this.carousel = new FCarousel(carouselOptions);
     this.init();
 }
+
+/**
+ * Perform some initialization.
+ */
+UnderlineSlide.prototype.init = function ()
+{
+    this.setIndexes();
+    this.gotoMenuItem(this.list.querySelector('.nav__list__item'), false);
+};
+
+/**
+ * Sets the necessary indexes in the menu items.
+ * 
+ */
+UnderlineSlide.prototype.setIndexes = function ()
+{
+    this.list.querySelectorAll('.nav__list__link').forEach((element, index) =>
+    {
+        element.dataset.carouselTargetItem = index;
+    });
+};
 
 /**
  * Goes to the next slide.
@@ -36,13 +58,13 @@ UnderlineSlide.prototype.gotoPreviousSlide = function ()
 };
 
 /**
- * The callback executed when a list item is clicked
+ * The callback that is executed when a list item is clicked
  * 
  * @param {Event} event The event generated.
  */
 UnderlineSlide.prototype.gotoMenuItemCB = function (event)
 {
-    const clickedAnchor = event.target.closest('.nav__list__link');
+    const clickedAnchor = event.target.closest('.nav__list__item');
     if (!clickedAnchor)
     {
         return;
@@ -52,24 +74,14 @@ UnderlineSlide.prototype.gotoMenuItemCB = function (event)
 };
 
 /**
- * Perform some initialization.
- */
-UnderlineSlide.prototype.init = function ()
-{
-    this.setIndexes();
-    this.moveUnderlineByIndex(0, false);
-};
-
-/**
- * Sets the necessary indexes in the menu items.
+ * Callback that is executed each time the carousel goes to a new slide.
  * 
+ * @param {Number} index The index of the new slide to go to.
  */
-UnderlineSlide.prototype.setIndexes = function ()
+UnderlineSlide.prototype.moveUnderlineByIndex = function(index)
 {
-    this.list.querySelectorAll('.nav__list__link').forEach((element, index) =>
-    {
-        element.dataset.carouselTargetItem = index;
-    });
+    const htmlLiElement = this.list.querySelector('[data-list-item-index="' + index + '"]');
+    this.moveUnderline(htmlLiElement);
 };
 
 /**
@@ -80,23 +92,21 @@ UnderlineSlide.prototype.setIndexes = function ()
  */
 UnderlineSlide.prototype.gotoMenuItem = function (menuItemElement, animate = true)
 {
-    this.carousel.gotoSlide(menuItemElement.dataset.carouselTargetItem);
+    this.carousel.gotoSlide(menuItemElement.dataset.listItemIndex, animate);
 };
 
-UnderlineSlide.prototype.moveUnderlineByIndex = function (menuItemIndex, animate = true)
+/**
+ * Detaches the underline from its current HTML container, getting its width and left values.
+ */
+UnderlineSlide.prototype.detachUnderline = function()
 {
-    if (!animate)
-    {
-        this.underline.style.transition = 'none';
-    }
-    const menuItemElement = this.list.querySelector('.nav__list__link[data-index="' + menuItemIndex + '"]');
-    this.moveUnderline(menuItemElement);
-
-    if (!animate)
-    {
-        this.underline.offsetHeight;
-        this.underline.style.transition = '';
-    }
+    const currentUnderlineHolder = this.underline.closest('[data-list-item-index]');
+    this.underline.style.transition = 'none';
+    this.underline.style.position = 'absolute';
+    this.underline.style.left = currentUnderlineHolder.offsetLeft + 'px';
+    this.underline.style.width = currentUnderlineHolder.offsetWidth + 'px';
+    this.underline.offsetHeight;
+    this.underline.style.transition = '';
 };
 
 /**
@@ -106,14 +116,38 @@ UnderlineSlide.prototype.moveUnderlineByIndex = function (menuItemIndex, animate
 UnderlineSlide.prototype.moveUnderline = function (menuItemElement)
 {
     this.list.querySelector('.nav__list__link--active').classList.remove('nav__list__link--active');
-    this.underline.style.transform = 'translateX(' + (menuItemElement.offsetLeft + 'px') + ')';
+    menuItemElement.querySelector('.nav__list__link').classList.add('nav__list__link--active');
+    
+    this.detachUnderline();
+
+    this.underline.dataset.currentParentIndex = menuItemElement.dataset.listItemIndex;
+    this.underline.style.left = menuItemElement.offsetLeft + 'px';
     this.underline.style.width = menuItemElement.offsetWidth + 'px';
-    menuItemElement.classList.add('nav__list__link--active');
 };
 
-UnderlineSlide.prototype.getCarousel = function()
+/**
+ * Inserts the underline into the parent specified in its 'current-parent-index' attribute
+ */
+UnderlineSlide.prototype.insertUnderlineIntoNewParent = function()
 {
-  return this.carousel;  
+    const newUnderlineParent = this.list.querySelector('.nav__list__item[data-list-item-index="' + this.underline.dataset.currentParentIndex + '"] .nav__list__underline_holder');
+    newUnderlineParent.appendChild(this.underline);
+    this.underline.style.position = 'static';
+    this.underline.style.left = '0';
+    this.underline.style.width = 'auto';
+};
+
+/**
+ * To be executed when the transition ends to insert the underline in its new parent.
+ * 
+ * @param {event} event The event triggered when the transition ends.
+ */
+UnderlineSlide.prototype.underlineTransitionEnd = function(event)
+{
+    if (event.propertyName === 'left') 
+    {
+        this.insertUnderlineIntoNewParent();
+    }
 };
 
 const underline = new UnderlineSlide();
