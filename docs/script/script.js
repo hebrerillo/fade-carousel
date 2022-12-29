f53ddce23942f30756b73adfd10fe8665c869cdd
hebrerillo <hebrerillo@gmail.com>
2022-12-29 17:57:15 +0100

    Adding carousel with their fading slides coming from the right side

--- docs/script/script.js
+++ docs/script/script.js
@@ -1,14 +1,44 @@
 'use strict';
 
+//Side carousel
 (function ()
 {
     let sideCarouselOptions = {
         carousel: '#sideCarousel',
         side: 'right',
         intervalDelay: 4000,
-        fadeInDuration: 2000
+        fadeInDuration: 2000,
+        onstart: selectDot
     };
 
-    new FCarousel(sideCarouselOptions);
+    let carousel = new FCarousel(sideCarouselOptions);
+    
+    let dotsContainer = document.querySelector('.dots');
+    dotsContainer.addEventListener('click', (event)=>
+    {
+       const dot = event.target.closest('.dot');
+       if (!dot)
+       {
+           return;
+       }       
+       carousel.gotoSlide(+dot.dataset.index);
+
+    });
+
+    function selectDot(dotIndex)
+    {
+        dotsContainer.querySelectorAll('.dot').forEach((dotElement, index)=>
+        {
+            if (dotIndex === index)
+            {
+                dotElement.classList.add('dot--selected');
+            }
+            else
+            {
+                dotElement.classList.remove('dot--selected');
+            }
+        });
+    }
+    
 })();
 
