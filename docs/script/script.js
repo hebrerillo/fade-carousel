70f388163dd814041fa7ea79e669216bdc696aa9
hebrerillo <hebrerillo@gmail.com>
2022-12-29 14:00:05 +0100

    Side right carousel

---
+++ docs/script/script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+(function ()
+{
+    let sideCarouselOptions = {
+        carousel: '#sideCarousel',
+        side: 'right',
+        intervalDelay: 4000
+    };
+
+    new FCarousel(sideCarouselOptions);
+})();
+
